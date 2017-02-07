6638a03cdede54b9870e58a34275a0c77f61a9d6
Steven Fippinger <sdfippinger@gmail.com>
2017-02-07 10:41:18 -0600

    Form updates with existing column classes.

--- plugin.js
+++ plugin.js
@@ -2,8 +2,11 @@
   'use strict'
 
   function Plugin (editor, url) {
-    this.editor = editor
-    this.url = url
+    var plugin = this;
+    plugin.editor = editor;
+    plugin.url = url;
+    plugin.data = [];
+
 
     this.generalItems = [
       {
@@ -36,6 +39,14 @@
             values: [{text: 'None', value: 'none'}].concat([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(function (width) {
               return { text: width, value: 'col-xs-'+width }
             })),
+            onPostRender: function() {
+              var match = jQuery.grep(plugin.data, function(value) {
+                return value.indexOf("col-xs-") >= 0;
+              });
+              if(match.length > 0) {
+                this.value(match[0]);
+              }
+            }
           },
           {
             type: 'ListBox', 
@@ -43,6 +54,14 @@
             values: [{text: 'None', value: 'none'}].concat([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(function (width) {
               return { text: width, value: 'col-xs-offset'+width }
             })),
+            onPostRender: function() {
+              var match = jQuery.grep(plugin.data, function(value) {
+                return value.indexOf("col-xs-offset-") >= 0;
+              });
+              if(match.length > 0) {
+                this.value(match[0]);
+              }
+            }
           }
         ]
       },
@@ -63,13 +82,29 @@
             values: [{text: 'None', value: 'none'}].concat([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(function (width) {
               return { text: width, value: 'col-sm-'+width }
             })),
+            onPostRender: function() {
+              var match = jQuery.grep(plugin.data, function(value) {
+                return value.indexOf("col-sm-") >= 0;
+              });
+              if(match.length > 0) {
+                this.value(match[0]);
+              }
+            }
           },
           {
             type: 'ListBox', 
             name: 'offset',  
             values: [{text: 'None', value: 'none'}].concat([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(function (width) {
-              return { text: width, value: 'col-sm-offset'+width }
+              return { text: width, value: 'col-sm-offset-'+width }
             })),
+            onPostRender: function() {
+              var match = jQuery.grep(plugin.data, function(value) {
+                return value.indexOf("col-sm-offset-") >= 0;
+              });
+              if(match.length > 0) {
+                this.value(match[0]);
+              }
+            }
           }
         ]
       },
@@ -90,6 +125,14 @@
             values: [{text: 'None', value: 'none'}].concat([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(function (width) {
               return { text: width, value: 'col-md-'+width }
             })),
+            onPostRender: function() {
+              var match = jQuery.grep(plugin.data, function(value) {
+                return value.indexOf("col-md-") >= 0;
+              });
+              if(match.length > 0) {
+                this.value(match[0]);
+              }
+            }
           },
           {
             type: 'ListBox', 
@@ -98,7 +141,12 @@
               return { text: width, value: 'col-md-offset-'+width }
             })),
             onPostRender: function() {
-              this.value('col-md-offset-5');
+              var match = jQuery.grep(plugin.data, function(value) {
+                return value.indexOf("col-md-offset-") >= 0;
+              });
+              if(match.length > 0) {
+                this.value(match[0]);
+              }
             }
           }
         ]
@@ -121,23 +169,39 @@
             values: [{text: 'None', value: 'none'}].concat([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(function (width) {
               return { text: width, value: 'col-lg-'+width }
             })),
+            onPostRender: function() {
+              var match = jQuery.grep(plugin.data, function(value) {
+                return value.indexOf("col-lg-") >= 0;
+              });
+              if(match.length > 0) {
+                this.value(match[0]);
+              }
+            }
           },
           {
             type: 'ListBox', 
             name: 'offset',  
             values: [{text: 'None', value: 'none'}].concat([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(function (width) {
-              return { text: width, value: 'col-lg-offset'+width }
+              return { text: width, value: 'col-lg-offset-'+width }
             })),
+            onPostRender: function() {
+              var match = jQuery.grep(plugin.data, function(value) {
+                return value.indexOf("col-lg-offset-") >= 0;
+              });
+              if(match.length > 0) {
+                this.value(match[0]);
+              }
+            }
           }
         ]
       }
     ]
 
-    this.init(editor, url)
+    this.init(editor, url);
   }
 
   Plugin.prototype.showDialog = function () {
-    var data = this.data
+    var data = this.data;
 
     this.editor.windowManager.open({
       title: 'Adjust Column Sizing',
@@ -173,6 +237,21 @@
 
   Plugin.prototype.init = function (editor, url) {
     var plugin = this
+
+    editor.on('NodeChange', function(e) {
+      if(e.selectionChange) {
+        $.each(e.parents, function(index, parent){
+          if(parent.nodeName === 'DIV') {   
+            if($(e.parents[index+1]).hasClass('row')) {
+              console.log(plugin.data)
+              // this.data = $(parent).attr("class").split(" ");
+              // console.log(this.data)
+              plugin.data = $(parent).attr("class").split(" ");
+            }
+          }
+        })
+      }
+    });
 
     // Add a button that opens a window
     editor.addButton('bscolumns', {
